91f567175b20554cdfdda246ae3aee165e041e3b
Jed Goh <electronfirework@gmail.com>
2024-10-03 15:10:34 +0800

    Added vec2 static constants

--- vector2.h
+++ vector2.h
@@ -75,5 +75,19 @@ namespace chiori
 			inOS << "vec2(" << inVec.x << ", " << inVec.y << ")";
 			return inOS;
 		}
+		// Static Vec2 properties
+		static const vec2 zero;
+		static const vec2 one;
+		static const vec2 left;
+		static const vec2 right;
+		static const vec2 up;
+		static const vec2 down;
 	};
+
+	const vec2 vec2::zero{ 0.0f, 0.0f };
+	const vec2 vec2::one{ 1.0f, 1.0f };
+	const vec2 vec2::down{ 0.0f, -1.0f };
+	const vec2 vec2::left{ -1.0f, 0.0f };
+	const vec2 vec2::right{ 1.0f, 0.0f };
+	const vec2 vec2::up{ 0.0f, 1.0f };
 }
