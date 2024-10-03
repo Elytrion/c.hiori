9baca36b7136225e292185ec361a2e4dde05d850
Jed Goh <electronfirework@gmail.com>
2024-10-03 15:10:03 +0800

    Updated PCH

--- pch.h
+++ pch.h
@@ -1,9 +1,32 @@
 #pragma once
 
 #include <iostream>
-#include <cmath>
-#include <string>
+#include <iomanip>
+#include <fstream>
+#include <memory>
+#include <utility>
 #include <algorithm>
+#include <functional>
+#include <string>
+#include <sstream>
+#include <filesystem>
 #include <limits>
-#include <inttypes.h>
+#include <random>
+#include <array>
+#include <vector>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
+#include <ctime>
+#include <type_traits>
+#include <cmath>
+#include <cstdint>
+
+#pragma warning(push, 0)
+
+// Windows
+#include <direct.h>
+#include <windows.h>
+#include <libloaderapi.h>
+#include <CommCtrl.h>
 #include "commons.h"
