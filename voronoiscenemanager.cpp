4350cf14e8880784b59f15003ad99d7ff2b8d6d8
Jed Goh <electronfirework@gmail.com>
2025-02-20 12:07:22 +0800

    Movable voronoi diagram done

--- voronoiscenemanager.cpp
+++ voronoiscenemanager.cpp
@@ -53,7 +53,8 @@ void VoronoiScene::Update(float dt)
         for (const auto& p : voronoi.v_points)
         {
             CP_Settings_Fill(CP_Color_Create(255, 255, 0, 255));
-            CP_Graphics_DrawCircle(p.x, p.y, 10);
+            CP_Settings_Stroke(CP_Color_Create(255, 255, 0, 255));
+            CP_Graphics_DrawCircle(p.x, p.y, 5);
         }
     }
 }
@@ -98,7 +99,7 @@ class GrowingScene : public VoronoiScene
     std::vector<cVec2> u_points;
     int numPoints = 100;
     int initPoints = 5;
-    float baseTimer = 0.1f;
+    float baseTimer = 0.05f;
     float timer = 0.0f;
 public:
     GrowingScene(DebugGraphics* drawer) : VoronoiScene(drawer) {}
@@ -114,8 +115,8 @@ public:
             else
                 points.push_back({ x, y });
         }
-        tris = cVoronoiDiagram::triangulateDelaunator(points);
-        voronoi.create(points.data(), points.size());
+        tris = cVoronoiDiagram::triangulateDelaunator(u_points);
+        voronoi.create(u_points.data(), u_points.size());
     }
 
     void Update(float dt) override
@@ -183,13 +184,15 @@ public:
             for (const auto& p : points)
             {
                 CP_Settings_Fill(CP_Color_Create(0, 255, 255, 255));
+                CP_Settings_Stroke(CP_Color_Create(0, 255, 255, 255));
                 CP_Graphics_DrawCircle(p.x, p.y, 10);
             }
             
             for (const auto& up : u_points)
             {
                 CP_Settings_Fill(CP_Color_Create(255, 255, 0, 255));
-                CP_Graphics_DrawCircle(up.x, up.y, 10);
+                CP_Settings_Stroke(CP_Color_Create(255, 255, 0, 255));
+                CP_Graphics_DrawCircle(up.x, up.y, 5);
             }
         }
     }
@@ -203,37 +206,156 @@ public:
     }
 };
 
-/*if (CP_Input_MouseDown(MOUSE_BUTTON_2))
+class ModifiableScene : public VoronoiScene
 {
-    cVec2 mousePos = { CP_Input_GetMouseX(), CP_Input_GetMouseY() };
-    if (selectedPoint)
+    std::vector<cVec2> points;
+    int numPoints = 100;
+    cVec2* selectedPoint{ nullptr };
+    cVec2 originalPoint{ cVec2::zero };
+    float baseTimer = 0.1f;
+    float timer = 0.0f;
+public:
+    ModifiableScene(DebugGraphics* drawer) : VoronoiScene(drawer) {}
+
+    void Load() override
     {
-        selectedPoint->x = mousePos.x;
-        selectedPoint->y = mousePos.y;
-        return;
+        int bufferEdgeWidth = 80;
+        for (int i = 0; i < numPoints; i++) {
+            float x = CP_Random_RangeInt(bufferEdgeWidth, CP_System_GetWindowWidth() - bufferEdgeWidth);
+            float y = CP_Random_RangeInt(bufferEdgeWidth, CP_System_GetWindowHeight() - bufferEdgeWidth);
+            points.push_back({ x, y });
+        }
+        tris = cVoronoiDiagram::triangulateDelaunator(points);
+        voronoi.create(points.data(), points.size());
     }
 
-    for (cVec2& pt : used_points)
+    void draw()
     {
-        if (distanceSqr(pt, mousePos) < 20.0f)
+        if (drawTriangles)
+        { // delaunay drawing
+            CP_Settings_StrokeWeight(1);
+            CP_Settings_Stroke(CP_Color_Create(255, 0, 0, 255)); // Red for Delaunay edges
+
+            for (const auto& tri : tris) {
+                cVec2 a = tri[0];
+                cVec2 b = tri[1];
+                cVec2 c = tri[2];
+                CP_Graphics_DrawLine(a.x, a.y, b.x, b.y);
+                CP_Graphics_DrawLine(b.x, b.y, c.x, c.y);
+                CP_Graphics_DrawLine(c.x, c.y, a.x, a.y);
+            }
+        }
+
+        if (drawVoronoi)
         {
-            selectedPoint = &pt;
-            originalPos = pt;
-            break;
+            for (const auto& vvertex : voronoi.vertices)
+            {
+                for (const auto& edgeIndex : vvertex.edgeIndices)
+                {
+                    const cVEdge& edge = voronoi.edges[edgeIndex];
+                    if (edge.infinite)
+                    {
+                        const cVec2& p1 = edge.origin;
+                        const cVec2& p2 = edge.origin + edge.endDir * 1000;
+                        CP_Settings_StrokeWeight(1);
+                        CP_Settings_Stroke(CP_Color_Create(0, 255, 255, 255)); // Green for Delaunay edges
+                        CP_Graphics_DrawLine(p1.x, p1.y, p2.x, p2.y);
+                        continue;
+                    }
+                    const cVec2& p1 = edge.origin;
+                    const cVec2& p2 = edge.endDir;
+                    CP_Settings_StrokeWeight(1);
+                    CP_Settings_Stroke(CP_Color_Create(255, 0, 255, 255)); // Green for Delaunay edges
+                    CP_Graphics_DrawLine(p1.x, p1.y, p2.x, p2.y);
+                }
+            }
+        }
+
+        if (drawPoints)
+        {
+            for (const auto& p : points)
+            {
+                CP_Settings_Fill(CP_Color_Create(255, 255, 0, 255));
+                CP_Graphics_DrawCircle(p.x, p.y, 10);
+            }
         }
     }
-}
-else if (CP_Input_MouseReleased(MOUSE_BUTTON_2))
-{
-    if (!selectedPoint)
-        return;
 
-    voronoi.remove(originalPos, false);
-    voronoi.add(*selectedPoint);
-    originalPos = cVec2::zero;
-    selectedPoint = nullptr;
-}*/
+    void Update(float dt) override
+    {
+        cVec2 mousePos = { CP_Input_GetMouseX(), CP_Input_GetMouseY() };
+        draw();
+        if (CP_Input_MouseDown(MOUSE_BUTTON_2))
+        {
+            if (!selectedPoint)
+            {
+                for (cVec2& pt : points)
+                {
+                    if (distanceSqr(pt, mousePos) < 25.0f) // 5 ^ 2
+                    {
+                        selectedPoint = &pt;
+                        originalPoint = pt;
+                        break;
+                    }
+                }
+                return;
+            }
+            
+            selectedPoint->x = mousePos.x;
+            selectedPoint->y = mousePos.y;
 
+            if (distanceSqr(*selectedPoint, originalPoint) <= 4.0f)
+                return; // hasn't move far enough to care about changing;
+            
+            timer -= dt;
+            if (timer <= 0.0f)
+            {
+                timer = baseTimer;
+                voronoi.remove(originalPoint, false);
+                voronoi.add(*selectedPoint);
+                originalPoint = *selectedPoint;
+            }
+        }
+        else if (CP_Input_MouseReleased(MOUSE_BUTTON_2))
+        {
+            if (!selectedPoint)
+                return;
+            
+            voronoi.remove(originalPoint, false);
+            voronoi.add(*selectedPoint);
+            originalPoint = cVec2::zero;
+            selectedPoint = nullptr;
+
+            return;
+        }
+
+        if (CP_Input_KeyTriggered(KEY_N))
+        {
+            points.push_back(mousePos);
+            voronoi.add(mousePos);
+        }
+        else if (CP_Input_KeyTriggered(KEY_M))
+        {
+            auto itr = std::find_if(points.begin(), points.end(), [&](const cVec2& p) {
+                return distanceSqr(p, mousePos) <= 4.0f;
+                });
+            if (itr == points.end())
+                return; // point not in diagram, ignore
+            cVec2 fpoint = *itr;
+            points.erase(itr);
+            voronoi.remove(fpoint);
+        }
+    }
+    
+
+    void Unload() override
+    {
+        VoronoiScene::Unload();
+        points.clear();
+        selectedPoint = nullptr;
+        originalPoint = cVec2::zero;
+    }
+};
 #pragma endregion
 
 VoronoiSceneManager::VoronoiSceneManager(DebugGraphics* drawer)
@@ -242,6 +364,7 @@ VoronoiSceneManager::VoronoiSceneManager(DebugGraphics* drawer)
 
     AddScene(new StaticScene(drawer));
     AddScene(new GrowingScene(drawer));
+    AddScene(new ModifiableScene(drawer));
 
     //ChangeScene(0);
 }
